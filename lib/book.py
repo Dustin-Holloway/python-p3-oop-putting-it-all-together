82c4251d29620c90db5d98a0ec5713091e103e63
Dustin <dustinhollowaypdx@gmail.com>
2023-05-04 14:42:43 -0700

    commit all

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Book:
-    pass
+    def __init__(self, page_count, title="And Then There Were None",  ):
+        self.page_count = page_count
+        self.title = title
+
+    def turn_page(self):
+        print ("Flipping the page...wow, you read fast!")
+
+
+    def set_page_count(self, count):
+        if isinstance(count, int):
+            self._page_count = count
+        else:
+            print("page_count must be an integer")
+            self._page_count = None
+
+    def get_page_count(self):
+        return self._page_count
+    
+    page_count = property(get_page_count, set_page_count)
+
+
+# mystory = Book(25)
+# mystory.turn_page()
+
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Shoe:
-    pass
+    def __init__(self, brand):
+        self.brand = brand
+
+
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")     
+
+    def get_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+            self._size = None
+
+    def set_size(self):
+        return self._size
+        
+    size = property(set_size, get_size)
